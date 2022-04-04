7a1d0174ada4f37dedfbd4a58755abed1a02f09a
Marcel Klehr <mklehr@gmx.net>
2022-04-04 19:44:53 +0200

    Add license and privacy

--- pages/license.vue
+++ pages/license.vue
@@ -2,55 +2,26 @@
   <div>
     <v-row justify="center" align="center">
       <v-col cols="10" sm="8">
-        <h1 class="headline">Download</h1>
+        <h1 class="headline">License</h1>
       </v-col>
     </v-row>
     <v-row justify="center" align="center">
-      <v-col cols="10" sm="8" class="d-flex flex-wrap">
-        <v-card class="mb-5 mr-5" rounded>
-          <v-card-title
-            ><v-icon size="50">mdi-firefox</v-icon> Firefox</v-card-title
-          >
-          <v-card-text><v-btn class="primary">Download</v-btn></v-card-text>
-        </v-card>
-        <v-card class="mb-5 mr-5" rounded>
-          <v-card-title
-            ><v-icon size="50">mdi-microsoft-edge</v-icon> Edge</v-card-title
-          >
-          <v-card-text><v-btn class="primary">Download</v-btn></v-card-text>
-        </v-card>
-        <v-card class="mb-5 mr-5" rounded>
-          <v-card-title
-            ><v-icon size="50">mdi-google-chrome</v-icon> Chrome</v-card-title
-          >
-          <v-card-text><v-btn class="primary">Download</v-btn></v-card-text>
-        </v-card>
-        <v-card class="mb-5 mr-5" rounded>
-          <v-card-title
-            ><v-icon size="50">mdi-android</v-icon> Android</v-card-title
-          >
-          <v-card-text class="d-flex flex-column"
-            ><v-btn class="primary">PlayStore</v-btn><br /><v-btn
-              class="primary mt-2"
-              >F-Droid</v-btn
-            ></v-card-text
-          >
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row justify="center" align="center">
-      <v-col cols="10" sm="8" class="d-flex flex-wrap">
-        <p>
-          Need help? Learn more on
-          <NuxtLink to="/guides">how to get started</NuxtLink>.
-        </p>
+      <v-col cols="10" sm="8" class="d-flex flex-wrap" v-html="license">
+
       </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
+import license from '~/license.md'
+
 export default {
-  name: 'DownloadPage',
+  name: 'LicensePage',
+  data() {
+    return {
+      license
+    }
+  }
 }
 </script>
--- pages/privacy.vue
+++ pages/privacy.vue
@@ -2,55 +2,26 @@
   <div>
     <v-row justify="center" align="center">
       <v-col cols="10" sm="8">
-        <h1 class="headline">Download</h1>
+        <h1 class="headline">Privacy</h1>
       </v-col>
     </v-row>
     <v-row justify="center" align="center">
-      <v-col cols="10" sm="8" class="d-flex flex-wrap">
-        <v-card class="mb-5 mr-5" rounded>
-          <v-card-title
-            ><v-icon size="50">mdi-firefox</v-icon> Firefox</v-card-title
-          >
-          <v-card-text><v-btn class="primary">Download</v-btn></v-card-text>
-        </v-card>
-        <v-card class="mb-5 mr-5" rounded>
-          <v-card-title
-            ><v-icon size="50">mdi-microsoft-edge</v-icon> Edge</v-card-title
-          >
-          <v-card-text><v-btn class="primary">Download</v-btn></v-card-text>
-        </v-card>
-        <v-card class="mb-5 mr-5" rounded>
-          <v-card-title
-            ><v-icon size="50">mdi-google-chrome</v-icon> Chrome</v-card-title
-          >
-          <v-card-text><v-btn class="primary">Download</v-btn></v-card-text>
-        </v-card>
-        <v-card class="mb-5 mr-5" rounded>
-          <v-card-title
-            ><v-icon size="50">mdi-android</v-icon> Android</v-card-title
-          >
-          <v-card-text class="d-flex flex-column"
-            ><v-btn class="primary">PlayStore</v-btn><br /><v-btn
-              class="primary mt-2"
-              >F-Droid</v-btn
-            ></v-card-text
-          >
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row justify="center" align="center">
-      <v-col cols="10" sm="8" class="d-flex flex-wrap">
-        <p>
-          Need help? Learn more on
-          <NuxtLink to="/guides">how to get started</NuxtLink>.
-        </p>
+      <v-col cols="10" sm="8" class="d-flex flex-wrap" v-html="privacy">
+
       </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
+import privacy from '~/privacy.md'
+
 export default {
-  name: 'DownloadPage',
+  name: 'PrivacyPage',
+  data() {
+    return {
+      privacy
+    }
+  }
 }
 </script>
