c04a423d6f63f8f1b1784c4b01a932f4ba67c232
Marcel Klehr <mklehr@gmx.net>
2022-04-04 17:24:55 +0200

    index: Make design responsive

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,23 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="10" sm="8">
-      <v-row>
-        <v-col cols="6">
+      <v-row class="d-block d-sm-flex">
+        <v-col class="col-12 col-sm-6">
           <v-img src="undraw_in_sync.svg" />
         </v-col>
-        <v-col cols="6" class="flex-column d-flex justify-center align-center">
+        <v-col class="col-12 col-sm-6 flex-column d-flex justify-center align-center">
           <div style="width: 75%">
             <h2 class="headline">Sync bookmarks privately</h2>
             <p class="subtitle-1">across browsers and devices</p>
-            <p class="mt-3">
+            <p class="mt-3 d-xl-block d-sm-flex flex-column">
               <v-btn class="primary" :to="{ name: 'download' }">Download</v-btn>
-              <v-btn class="ml-2" :to="{ name: 'guides' }">Get started</v-btn>
+              <v-btn class="ml-xl-2 mt-sm-2 mt-xl-0" :to="{ name: 'guides' }">Get started</v-btn>
             </p>
           </div>
         </v-col>
       </v-row>
-      <v-row class="mt-12">
-        <v-col cols="6" class="flex-column d-flex justify-center">
+      <v-row class="mt-12 d-block d-sm-flex">
+        <v-col class="col-12 col-sm-6 flex-column d-flex justify-center">
           <h2 class="headline">
             Browser Extensions & Android App that sync your bookmarks across all
             your browsers and devices
@@ -27,27 +27,27 @@
             optionally end-to-end encrypted via Google Drive
           </p>
         </v-col>
-        <v-col cols="6">
+        <v-col class="col-12 col-sm-6">
           <v-img src="floccus_schema.png" />
         </v-col>
       </v-row>
-      <v-row class="mt-12">
-        <v-col cols="6" class="d-flex flex-column justify-center">
-          <v-card>
+      <v-row class="mt-12 d-block d-sm-flex">
+        <v-col class="col-12 col-sm-6 d-flex flex-column justify-center">
+          <v-card style="min-height: 50%;">
             <Timeline id="floccusAddon" error-message="This tweet could not be loaded" source-type="profile" :options="{ tweetLimit: '1' }">
               <v-card-text><a href="https://twitter.com/floccusAddon">Loading tweets</a></v-card-text>
             </Timeline>
           </v-card>
         </v-col>
-        <v-col cols="6">
+        <v-col class="col-12 col-sm-6">
           <v-img src="undraw_devices.svg" />
         </v-col>
       </v-row>
-      <v-row class="mt-12">
-        <v-col cols="6">
+      <v-row class="mt-12 d-block d-sm-flex">
+        <v-col class="col-12 col-sm-6 ">
           <v-img src="undraw_team.svg" />
         </v-col>
-        <v-col cols="6" class="flex-column d-flex justify-center">
+        <v-col class="col-12 col-sm-6 flex-column d-flex justify-center">
           <h2 class="headline">
             Use your own skills to make floccus even better!
           </h2>
@@ -71,27 +71,29 @@
           </h3>
         </v-col>
       </v-row>
-      <v-row class="mt-12">
-        <v-col cols="6" class="flex-column d-flex justify-center">
+      <v-row class="mt-12 d-block d-sm-flex">
+        <v-col class="col-12 col-sm-6 flex-column d-flex justify-center">
           <h2 class="headline">Support floccus</h2>
           <p class="subtitle-1">
             Give something back to the creator of floccus. Any amount, no matter
             how small, is appreciated.
           </p>
-          <v-btn href="https://github.com/sponsors/marcelklehr" class="mt-2"
-            >GitHub Sponsors</v-btn
-          >
-          <v-btn href="https://liberapay.com/marcelklehr/donate" class="mt-2"
-            >Liberpay</v-btn
-          >
-          <v-btn href="https://opencollective.com/floccus" class="mt-2"
-            >OpenCollective</v-btn
-          >
-          <v-btn href="https://www.paypal.me/marcelklehr1" class="mt-2"
-            >PayPal</v-btn
-          >
+          <div class="d-flex flex-wrap flex-column flex-lg-row">
+            <v-btn href="https://github.com/sponsors/marcelklehr" class="mt-2 mr-2 col-lg-5"
+              >GitHub Sponsors</v-btn
+            >
+            <v-btn href="https://liberapay.com/marcelklehr/donate" class="mt-2 mr-2  col-lg-5"
+              >Liberpay</v-btn
+            >
+            <v-btn href="https://opencollective.com/floccus" class="mt-2 mr-2  col-lg-5"
+              >OpenCollective</v-btn
+            >
+            <v-btn href="https://www.paypal.me/marcelklehr1" class="mt-2 mr-2  col-lg-5"
+              >PayPal</v-btn
+            >
+          </div>
         </v-col>
-        <v-col cols="6">
+        <v-col class="col-12 col-sm-6">
           <v-img src="undraw_love.svg" />
         </v-col>
       </v-row>
