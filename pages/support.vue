de9ec7890d5f795f8463216e34817c9496203de1
Marcel Klehr <mklehr@gmx.net>
2022-06-02 14:03:10 +0200

    Add support page for ios

---
+++ pages/support.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-row justify="center" align="center">
+      <v-col cols="10" sm="8">
+        <h1 class="headline">Support</h1>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col cols="10" sm="8">
+        <p>If you require support you can</p>
+        <ul>
+          <li><a href="https://github.com/floccusaddon/floccus/issues">Submit an issue on GitHub</a></li>
+          <li><a href="mailto:mklehr@gmx.net">Send a mail</a></li>
+        </ul>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col cols="10" sm="8">
+        <p>
+          Need help? Learn more on
+          <NuxtLink :to="{ name: 'guides' }">how to get started</NuxtLink>.
+        </p>
+        <p>
+          How does Floccus deal with your personal data?
+          <NuxtLink to="/privacy">Privacy Policy</NuxtLink>
+        </p>
+        <p>
+          What are your rights when using floccus?
+          <NuxtLink to="/license">License</NuxtLink>
+        </p>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SupportPage',
+  head: {
+    title: 'Support',
+  },
+}
+</script>
