9a3114f0a3d194dc264819dc13306dd5b786316b
Marcel Klehr <mklehr@gmx.net>
2025-08-19 19:23:50 +0200

    feat(error): Beautify error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,12 +1,21 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
+    <v-row justify="center" align="center">
+      <v-col cols="10" sm="8">
+        <v-row class="d-flex flex-column align-center">
+          <v-img src="/undraw_taken.svg" max-width="400" />
+        </v-row>
+        <v-row class="d-flex flex-column align-center mt-10">
+          <h1 v-if="error.statusCode === 404">
+            {{ pageNotFound }}
+          </h1>
+          <h1 v-else>
+            {{ otherError }}
+          </h1>
+          <NuxtLink to="/">Go back home</NuxtLink>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-app>
 </template>
 
